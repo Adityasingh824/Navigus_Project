6eaca782cdca8cf881080bb38a01418b020f23f5
Aditya Singh <adityakrg3@gmail.com>
2021-09-23 01:43:32 +0530

    Backend and Frontend basic setup done and linked with Database

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      name:"",
+      email:"",
+      role:"",
+      password:"",
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e){
+    let name = e.target.name;
+    let value = e.target.value;
+    if(name=='name'){
+      this.setState({name:value});
+    }
+    if(name=='email'){
+      this.setState({email:value});
+    }
+    if(name=='role'){
+      this.setState({role:value});
+    }
+    if(name=='password'){
+      this.setState({password:value});
+    }
+  }
+  handleSubmit(){
+    let userData = {};
+    userData['userName'] = this.state.name;
+    userData['userEmail'] = this.state.email;
+    userData['userType'] = this.state.role;
+    userData['userPassword'] = this.state.password;
+    console.log(userData);
+    axios.post('http://localhost:5000/v1/user',userData).then(res=>{
+      console.log(res);
+    })
+
+  }
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <input type="text" name='name' value={this.state.name} placeholder="name" onChange={this.handleChange}/>
+          <input type="text" name='email' value={this.state.email} placeholder="email"  onChange={this.handleChange}/>
+          <input type="text" name='role' value={this.state.role} placeholder="role"  onChange={this.handleChange}/>
+          <input type="text" name='password' value={this.state.password} placeholder="password"  onChange={this.handleChange}/>
+          <button onClick = {this.handleSubmit}>Submit</button>
+  
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,135 @@
-const express = require("express");
+const express = require('express')
+const bodyParser = require('body-parser') 
+const mongoose = require("mongoose"); 
+const cors = require('cors');
+
+
 const app = express();
-app.get('/',(req,res)=>{
-    res.send('Hello Aditya Lets do it');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000
+
+mongoose.connect("mongodb+srv://Aditya:Aditya%40123@cluster0.ankjj.mongodb.net/Database?retryWrites=true&w=majority");
+
+const userSchema= new mongoose.Schema({
+    userName:{
+        type:String,
+        required:true
+    },
+    userEmail:{
+        type:String,
+        required:true
+    },
+    userType:{
+        type:String,
+        required:true
+    },
+    userPassword:{
+        type:String,
+        required:true
+    },
+
 });
 
-app.listen(5000,()=>{
-    console.log('Server is listening on Port 5000');
+const subjectSchema= new mongoose.Schema({
+    subjectName:{
+        type:String,
+        required:true
+    },
+    subjectQuestions:{
+        type:Object,
+    },
+    subjectAnswers:{
+        type:Object,
+    },
+
+});
+
+const User = new mongoose.model("User",userSchema);
+const Subject = new mongoose.model("Subject",subjectSchema);
+
+
+app.get('/v1/user', (req, res) => {
+  User.find({},function (err, users){
+      if(err){
+        console.log(err);
+      }else{
+        if(users.length>0)
+        {
+          res.json(users);
+        }
+      }
+  })
+});
+
+app.post('/v1/subject/create',function(req,res){
+  const newCollection = new Subject
+  ({
+      subjectName:req.body.subjectName,
+      subjectQuestions:req.body.subjectQuestions,
+      subjectAnswers:req.body.subjectAnswers
+  })
+  newCollection.save(function(err){
+      if(err){
+          console.log(err);
+      }else{
+          res.send(newCollection).status(200);
+      }
+  })
+});
+
+app.post('/v1/user',function(req,res){
+    const newCollection = new User
+    ({
+        userType:req.body.userType,
+        userEmail:req.body.userEmail,
+        userName:req.body.userName,
+        userPassword:req.body.userPassword,
+    })
+    newCollection.save(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.send(newCollection).status(200);
+        }
+    })
+  });
+
+// app.get('/v1/attributes/:id', (req, res) => {
+//   const id_=req.params.id;
+//   Attribute.findById(id_, (err, attribute) => {
+//       if(err){
+//         console.log(err);
+//       }else{
+//         if(attribute)
+//         {
+//           res.json(attribute);
+//         }
+//       }
+//   })
+// });
+
+// app.patch('/v1/attributes/:id',function(req,res){
+//   const id_=req.params.id;
+//   const update=req.body;
+//   console.log(update);
+//   Attribute.findByIdAndUpdate(id_,update,{new:true},function(err){
+//     if(err){
+//       console.log(err);
+//     }else{
+//       res.send("All is updated").status(200);
+//     }
+//   })
+// })
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
